f98c72f076d0542a35145e35cc33c6d898b3fe90
Juan Camilo Noriega Hoyos <jcnorieg@bancolombia.com.co>
2023-12-14 15:40:12 -0500

    carga de proyecto

---
+++ src/main/java/com/creacionjuan/sqslistener/SqslistenerApplication.java
@@ -0,0 +1,13 @@
+package com.creacionjuan.sqslistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqslistenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqslistenerApplication.class, args);
+	}
+
+}
